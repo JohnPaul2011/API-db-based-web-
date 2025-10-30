6dca0922e1544d35a2efe848bb5a50aed242453c
JPLinson1828 <94855398+JohnPaul2011@users.noreply.github.com>
2025-10-30 20:23:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,29 @@
 from flask import Flask, jsonify
-import uuid, time
+import uuid, time, threading, os
 from gevent import pywsgi
+from ping3 import ping
 
 app = Flask(__name__)
 
 storage = {}
+
+site = os.environ.get("API_KEY", "0.0.0.0")
+interval = 5  # seconds
+
+def auto_ping():
+    while True:
+        latency = ping(site)
+        timestamp = time.strftime('%Y-%m-%d %H:%M:%S')
+        uid = str(uuid.uuid4())[:8]
+        storage.setdefault("ping", {})
+        storage["ping"][uid] = {
+            "site": site,
+            "latency_ms": None if latency is None else round(latency * 1000, 2),
+            "time": timestamp
+        }
+        time.sleep(interval)
+        print("pinging")
+
 
 @app.route('/<user_id>/post/<data>', methods=['GET', 'POST'])
 def post_data(user_id, data):
@@ -25,6 +44,7 @@ def get_data(user_id):
     return jsonify(storage.get(user_id, {}))
 
 if __name__ == '__main__':
+    threading.Thread(target=auto_ping, daemon=True).start()
     server = pywsgi.WSGIServer(('0.0.0.0', 10000), app)
     print("Running on port 10000...")
     server.serve_forever()
